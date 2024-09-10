8744dbda5177b7de8efc1f3b0fbf217c71441b27
203422 <203422@ids.upchiapas.edu.mx>
2024-09-09 21:19:35 -0600

    Peticiones http

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/screens/dio_screen.dart';
+import 'package:flutter_application_1/screens/http_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,11 +13,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Container(),
+      home: const DioScreen(),
     );
   }
 }
